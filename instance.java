d4927ccad7a33a318db1067eeb57c6dfa690a33a
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-08-10 07:19:33 +0000

    Entering string

---
+++ instance.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class instance{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        
+        
+
+        
+        System.out.println("Enter a String");
+        String f=s.next();
+        System.out.println(f);
+        
+    }
+}
