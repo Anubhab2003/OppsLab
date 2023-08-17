6df237704f4252e88b769d3dec44f9edce56398a
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-08-17 07:50:30 +0000

    Test file

--- Test.java
+++ Test.java
@@ -1,11 +1,14 @@
 class Test{
     int a=15;
+    static int b=10;
     void show(){//Instance Methord
         System.out.println(a);
+        System.out.println(a+b);
     }
     public static void main(String args[]){
         Test t=new Test();//Creating a t object
         System.out.println(t.a);
         t.show();
+        System.out.println((t.a)+b);
     }
 }
