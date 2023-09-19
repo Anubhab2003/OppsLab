60696f50187455a58834c1c1b0d73d79525be18e
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-09-19 08:10:19 +0000

    Comaparing two string

---
+++ compare.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class compare{
+    public static void main(String args[]){
+        String str1="Hello";
+        String str2="hello";
+        System.out.println(str1.compareTo(str2));
+    }
+}
