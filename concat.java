e6bc68aaa23bbbd846c71187dd01762e3759f5bc
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-09-19 08:04:53 +0000

    Concatination of two string

---
+++ concat.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class concat{
+    public static void main(String args[]){
+        String str1="Chandrayaan";
+        String str2=" 3";
+        System.out.println(str1.concat(str2));
+    }
+}
