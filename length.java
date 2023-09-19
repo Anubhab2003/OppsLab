312bfa6063156a733f463d0d8769701833d98bdb
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-09-19 07:58:42 +0000

    Length of string

---
+++ length.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class length{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        System.out.println(str.length());
+    }
+}
