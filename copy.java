861378a6713d5062ca9f26a6f09f7a1e66fe1675
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-09-19 08:02:33 +0000

    Copying a string

---
+++ copy.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class copy{
+    public static void main(String args[]){
+        String str1="Hello";
+        String str2;
+        str2=str1;
+        System.out.println(str2);
+    }
+}
